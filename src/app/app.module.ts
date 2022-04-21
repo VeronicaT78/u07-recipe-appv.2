12c2bf52f62937e5f1a68bd49a7394fd7d15c8b9
VeronicaT78 <vthapper@hotmail.com>
2022-04-21 16:26:45 +0200

    basic component structure for recipe and favourite

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeCardComponent } from './components/recipes/recipe-card/recipe-card.component';
+import { FavouriteListComponent } from './components/favourites/favourite-list/favourite-list.component';
+import { FavouriteEditComponent } from './components/favourites/favourite-edit/favourite-edit.component';
+import { UserLoginComponent } from './components/user/user-login/user-login.component';
+import { UserSignupComponent } from './components/user/user-signup/user-signup.component';
+import { UserAccountComponent } from './components/user/user-account/user-account.component';
+import { RecipeViewComponent } from './components/recipes/recipe-card/recipe-view/recipe-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeCardComponent,
+    FavouriteListComponent,
+    FavouriteEditComponent,
+    UserLoginComponent,
+    UserSignupComponent,
+    UserAccountComponent,
+    RecipeViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/favourites/favourite-edit/favourite-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-favourite-edit',
+  templateUrl: './favourite-edit.component.html',
+  styleUrls: ['./favourite-edit.component.css']
+})
+export class FavouriteEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

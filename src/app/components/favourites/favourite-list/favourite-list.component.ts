4f4cf4ec31cd00efaa905810c9048b32c7ec2215
VeronicaT78 <vthapper@hotmail.com>
2022-04-22 15:21:53 +0200

    add favourite list

--- src/app/components/favourites/favourite-list/favourite-list.component.ts
+++ src/app/components/favourites/favourite-list/favourite-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Favourite } from '../../../models/favourites.model';
+
 @Component({
   selector: 'app-favourite-list',
   templateUrl: './favourite-list.component.html',
   styleUrls: ['./favourite-list.component.css']
 })
 export class FavouriteListComponent implements OnInit {
+  favourites: Favourite[] = [
+    new Favourite(5, 'mittsparaderecept', 'urlforpicture'),
+    new Favourite(4, 'mittsparaderecept', 'urlforpicture'),
+    new Favourite(3, 'mittsparaderecept', 'urlforpicture')
+  ];
 
   constructor() { }
 

bb37f2d7cee5682ff189d0412db7921264cba247
VeronicaT78 <vthapper@hotmail.com>
2022-04-22 15:22:48 +0200

    add basic card styling with dummy data

--- src/app/components/recipes/recipe-card/recipe-card.component.ts
+++ src/app/components/recipes/recipe-card/recipe-card.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './../../../models/recipe.model'
+
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.css']
 })
 export class RecipeCardComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('my recipe', 3, 30, 'imgurl', 'summary', 'instructions', ['sallad, sallad']),
+    new Recipe('my recipe', 3, 30, 'imgurl', 'summary', 'instructions', ['sallad, sallad']),
+    new Recipe('my recipe', 3, 30, 'imgurl', 'summary', 'instructions', ['sallad, sallad']),
+    new Recipe('my recipe', 3, 30, 'imgurl', 'summary', 'instructions', ['sallad, sallad'])
+  ];
 
   constructor() { }
 

f4a839e63d3916d7f6abea46c4e90a48c6a3351c
VeronicaT78 <vthapper@hotmail.com>
2022-04-25 12:24:33 +0200

    add eventemitter to header select

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  //spara valen och skicka över data till recipes, inuti recipes görs api requesten
+
+
+  onSelect(feature: string){
+    this.featureSelected.emit(feature);
   }
 
 }
